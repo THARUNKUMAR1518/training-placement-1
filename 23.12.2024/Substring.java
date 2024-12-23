f88314970609dd43756ab1441ef78aae2cb5e394
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2024-12-23 19:43:59 +0530

    Substring.java

---
+++ 23.12.2024/Substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        
+        List<Integer> result = new ArrayList<>();
+
+        if (s == null || s.isEmpty() || words == null || words.length == 0) {
+            return result;
+        }
+
+        int wordLength = words[0].length();
+        int wordCount = words.length;
+
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLength; i++) {
+            int left = i;
+            int right = i;
+            int count = 0;
+            Map<String, Integer> currentMap = new HashMap<>();
+
+            while (right + wordLength <= s.length()) {
+                String word = s.substring(right, right + wordLength);
+                right += wordLength;
+
+                if (wordMap.containsKey(word)) {
+                    currentMap.put(word, currentMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (currentMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLength);
+                        currentMap.put(leftWord, currentMap.get(leftWord) - 1);
+                        count--;
+                        left += wordLength;
+                    }
+
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    currentMap.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
